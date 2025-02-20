cc5273fa137f93e256b11cb8c2a3893198b4b3aa
Rajan <krajan92946@gmail.com>
2025-02-21 03:58:06 +0530

    persit store done

--- client/src/Redux/store.jsx
+++ client/src/Redux/store.jsx
@@ -4,10 +4,41 @@ import { configureStore } from "@reduxjs/toolkit";
 import myReducer from "./cardSlice"
 import myWishlist from "./wishlistSlice";
 
+
+// persist
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+const addtocart = persistReducer(persistConfig, myReducer)
+const  addtowishlist= persistReducer(persistConfig, myWishlist)
+
 const store=configureStore({
     reducer:{
-        mycart:myReducer,
-        wishlist:myWishlist,
+        mycart:addtocart,
+        wishlist:addtowishlist,
     }
 })
 export default store
+export const persistore=persistStore(store);
+
+
+
+
+// another method but it will all remoce from the card when you refresh
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import myReducer from "./cardSlice"
+// import myWishlist from "./wishlistSlice";
+
+// const store=configureStore({
+//     reducer:{
+//         mycart:myReducer,
+//         wishlist:myWishlist,
+//     }
+// })
+// export default store
